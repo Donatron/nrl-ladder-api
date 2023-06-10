c376b8c26aa506edfd0c6fbd475e07a1c1506245
donmacarthur <don@bluemeg.com>
2023-06-10 10:46:37 +0700

    move puppeteer invocation inside try catch block

--- routers/webScrapingRouter.js
+++ routers/webScrapingRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router();
 
-const WebScrapingController = require('../contollers/WebScrapingController')
+const WebScrapingController = require('../controllers/WebScrapingController')
 
 router.get('/', WebScrapingController.getOfficialLadder)
 
